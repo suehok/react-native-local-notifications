ce8453839e52bde79b13938124f43894ac12d1e4
sue hok <suehok@gmail.com>
2019-05-17 15:17:36 +0800

    add title

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import {NativeModules} from 'react-native';
 
 var RNLocalNotifications = {
-  createNotification: function(id, text, datetime, sound, hiddendata='') {
+  createNotification: function(id, title, text, datetime, sound, hiddendata='') {
         NativeModules.RNLocalNotifications.createNotification(id,title, text, datetime, sound, hiddendata);
   },
   deleteNotification: function(id) {
         NativeModules.RNLocalNotifications.deleteNotification(id);
   },
-  updateNotification: function(id, text, datetime, sound, hiddendata='') {
+  updateNotification: function(id, title, text, datetime, sound, hiddendata='') {
         NativeModules.RNLocalNotifications.updateNotification(id,title, text, datetime, sound, hiddendata);
   },
   setAndroidIcons: function(largeIconName, largeIconType, smallIconName, smallIconType) {
